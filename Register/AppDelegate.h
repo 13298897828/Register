9a0edc1c096284ffd6732666934963db50e4ea80
Tianqi Zhang <1120404010@qq.com>
2015-08-31 19:50:47 +0800

    Initial Commit

---
+++ Register/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Register
+//
+//  Created by 张天琦 on 15/8/31.
+//  Copyright (c) 2015年 张天琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Register/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Register
+//
+//  Created by 张天琦 on 15/8/31.
+//  Copyright (c) 2015年 张天琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
